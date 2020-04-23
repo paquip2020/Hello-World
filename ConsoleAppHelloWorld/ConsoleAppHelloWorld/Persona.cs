3a13d2c762a128e18fa659693c138482dc3e85fd
Francisca Palomino Palomino <profesor.franciscapp@iesazarquiel.es>
2020-04-23 21:09:12 +0200

    Clase Persona

---
+++ ConsoleAppHelloWorld/ConsoleAppHelloWorld/Persona.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ConsoleAppHelloWorld
+{
+    class Persona
+    {
+        public int MyProperty { get; set; }
+    }
+}
